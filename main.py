fc31a0e14ef7aa839275d51e086c8ab506531ad8
Noah <noahhodge2@gmail.com>
2020-12-05 21:56:27 -0800

    commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Python investing program to balance asset classes within a portfolio
+
+class Asset:
+
+    def __init__(self):
+        self.asset_class = None
+        self.value = None
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("lets go")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
