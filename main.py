3fa0102b79843d0b11a5feb6430ee4cd70787a95
Noah <noahhodge2@gmail.com>
2020-12-05 22:02:26 -0800

    commit

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 # Python investing program to balance asset classes within a portfolio
 
+class Portfolio:
+
+    def __init__(self):
+        self.market_value = 0
+        self.asset_distribution = {
+            'equity': 0,
+            'fixed_income': 0,
+            'real_estate': 0,
+        }
+        self.country_allocation = {
+            
+        }
+
+
+
 class Asset:
 
     def __init__(self):
