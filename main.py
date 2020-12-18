58409dba0cd25e136d8cbd9783ccc4fb7ae1a087
Noah <noahhodge2@gmail.com>
2020-12-17 17:33:53 -0800

    asset_distribution

--- main.py
+++ main.py
@@ -12,14 +12,17 @@ asset_one = Asset(1000, 'equity', 'US', .1, .4)
 asset_two = Asset(4000, 'equity', 'EM', .06, .6)
 asset_three = Asset(1000, 'fixed-income', 'US', .01, .1)
 asset_four = Asset(2000, 'fixed-income', 'US', .01, .1)
-
+asset_five = Asset(5000, 'fixed-income', 'US', .01, .1)
 
 liability_one = Liability(1500, .03, 30)
+liability_two = Liability(1500, .03, 30)
 
 my_portfolio.add_asset(asset_one)
 my_portfolio.add_asset(asset_two)
 my_portfolio.add_asset(asset_three)
 my_portfolio.add_asset(asset_four)
+my_portfolio.add_asset(asset_five)
 my_portfolio.add_liability(liability_one)
 
 print(my_portfolio.asset_allocation())
+print(my_portfolio.net_worth())
