1910caaf6b6c70b35bca67200565aa66a4451e55
Noah <noahhodge2@gmail.com>
2020-12-18 16:31:35 -0800

    df_functions

--- df_functions.py
+++ df_functions.py
@@ -4,3 +4,10 @@ def percentage_country(portfolio):
 
 def percentage_asset(portfolio):
     return portfolio.groupby("Type")["Value"].sum() / portfolio["Value"].sum()
+
+
+def total_risk(portfolio):
+    return portfolio["Risk"].sum() / len(portfolio['Risk'].index)
+
+def potential_annual_return(portfolio):
+    return (portfolio["Return"] * (portfolio["Value"]/portfolio["Value"].sum())).sum()
--- main.py
+++ main.py
@@ -5,15 +5,15 @@
 # ***pack annual expenses into one liability
 from portfolio import Portfolio
 from assets_liabilities import Asset, Liability
-from df_functions import percentage_country, percentage_asset
+from df_functions import percentage_country, percentage_asset, total_risk, potential_annual_return
 
 my_portfolio = Portfolio()
 
-asset_one = Asset(1000, 'equity', 'US', .1, .4)
-asset_two = Asset(4000, 'equity', 'US', .06, .6)
+asset_one = Asset(1000, 'equity', 'US', .1, .1)
+asset_two = Asset(1000, 'equity', 'US', .12, .2)
 asset_three = Asset(1000, 'equity', 'US', .01, .1)
 asset_four = Asset(2000, 'fixed-income', 'EM', .01, .1)
-asset_five = Asset(5000, 'fixed-income', 'US', .01, .1)
+asset_five = Asset(5000, 'equity', 'US', .01, .1)
 
 liability_one = Liability(1500, .03, 30)
 liability_two = Liability(6500, .03, 30)
@@ -29,8 +29,9 @@ my_portfolio.add_asset(asset_five)
 # portfolio_dataframe = PortfolioDataFrame(my_portfolio.generate_dataframe())
 pd = my_portfolio.generate_dataframe()
 
-print(percentage_country(pd))
-print(percentage_asset(pd))
-
+#print(f"\nAssets By Country: {percentage_country(pd)} \n")
+#print(f"Asset Allocation: {percentage_asset(pd)} \n")
+#print(f"Total Risk of Assets: {total_risk(pd)}")
+print(f"Potential Return of Assets: {potential_annual_return(pd)}")
 
 
