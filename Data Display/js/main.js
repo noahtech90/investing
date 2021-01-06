19a9acb4b1f38f2c1454cd1c13addf9e20e387fe
Noah <noahhodge2@gmail.com>
2021-01-05 16:53:41 -0800

    asset creation

--- Data Display/js/main.js
+++ Data Display/js/main.js
@@ -7,3 +7,19 @@ class Asset {
             this.risk = risk
     }
 }
+
+assetList = [];
+
+var form = document.querySelector("asset-form");
+form.onsubmit = function () {
+
+    value = document.getElementById("value").value
+    assetClass = document.getElementById("type").value
+    country = document.getElementById("country").value;
+    annual_return = .10
+    risk = .07
+
+    newAsset = new Todo(assetClass, country, annual_return, risk)
+    assetList.push(newTask)
+    console.log(assetList)
+}
