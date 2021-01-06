a4bd2d0e01f1c3d75467c3d658fb4ea0cebfc595
Noah <noahhodge2@gmail.com>
2021-01-05 16:48:00 -0800

    javascript asset

---
+++ Data Display/js/main.js
@@ -0,0 +1,9 @@
+class Asset {
+    constructor(value, assetClass, country, annual_return, risk) {
+        this.value = value,
+            this.assetClass = assetClass,
+            this.country = country,
+            this.annual_return = annual_return,
+            this.risk = risk
+    }
+}
